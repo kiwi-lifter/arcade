158fcb348b2b676c1cfe5347644e4ebfa6cfd107
Garrick <cerulean808@hotmail.com>
2016-06-24 08:26:55 +1200

    added functionality to stop player falling off board, returns to start after completion of level

--- js/app.js
+++ js/app.js
@@ -8,11 +8,16 @@ var bottomBorder = rowsNum * rowHeight;
 
 var enemyPopulation =3;
 
+var playerHeight = 171;
+var playerWidth = 101;
+
+var playerXcoord =  (colsNum * colWidth)/2 - playerWidth/2;
+var playerYcoord =  (rowsNum * rowHeight) - playerHeight/2;
+
 // y coordinates for pavement rows
 var pavementRows = [ 60, 145, 230];
 // enemy speeds
 var speeds = [150, 250, 350];
-
 
 // Enemies our player must avoid
 var Enemy = function() {
@@ -23,7 +28,7 @@ var Enemy = function() {
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
 	// random enemy start
-	this.setStartEnemy();	
+	this.startEnemy();	
 };
 
 // Update the enemy's position, required method for game
@@ -36,9 +41,27 @@ Enemy.prototype.update = function(dt) {
 	
 	// re start the enemy once it reaches end of row
 	if(this.x > rightBorder) {
-		this.setStartEnemy();
+		this.startEnemy();
 	}
 	
+};
+
+// restart Player if hit by enemy
+Enemy.prototype.collidingWithPlayer = function () {
+    if (player.x <= this.x + 50 &&
+        this.x <= player.x + 20 &&
+        player.y <= this.y + 20 &&
+        this.y <= player.y + 20) {
+
+        this.startEnemy();
+    }
+};
+
+// reset the enemy position once it reaches end of row
+Enemy.prototype.startEnemy = function() {
+	this.x = -colWidth;
+	this.y = randomNumFromArray(pavementRows);
+	this.speed = randomNumFromArray(speeds);
 };
 
 // Draw the enemy on the screen, required method for game
@@ -46,52 +69,60 @@ Enemy.prototype.render = function() {
 	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-
-// reset the enemy position once it reaches end of row
-Enemy.prototype.setStartEnemy = function() {
-	this.x = -colWidth;
-	this.y = randomNumFromArray(pavementRows);
-	this.speed = randomNumFromArray(speeds);
-};
-
-
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 var Player = function(){
 	this.sprite = 'images/char-boy.png';
+	this.startPlayer();
 	
 };
 
 Player.prototype.update = function() {
-	//console.log("update function here");
-} 
+	console.log(this.y);
+	if (this.y < 0) {	
+        this.startPlayer();
+    }
+};
 
 Player.prototype.render = function() {
-	//console.log("render function here");
-} 
+	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}; 
 
-Player.prototype.handleInput = function(allowedKeys) {
-	//console.log("handleInput function here");
-	switch(allowedKeys){
-	case "left":
-	this.x += this.x - 100;
-	break;
-	
-	case "up":
-	this.x += this.y - 100;
-	break;
-	
-	case "right":
-	this.y += this.x + 100;
-	break;
-	
-	case "down":
-	this.y += this.y + 100;
-	break;
+Player.prototype.startPlayer = function(){
+	this.x = playerXcoord;
+	this.y = playerYcoord;
+};
 
-    }
-} 
+Player.prototype.handleInput = function(keyCode) {
+	
+		switch(keyCode){
+		
+			case "left":
+			if(this.x>0){
+				this.x = this.x - colWidth;
+			};
+			break;
+			
+			case "up":
+			if(this.y>0){
+				this.y = this.y - rowHeight;
+			};
+			break;
+			
+			case "right":
+			if(this.x<400){
+				this.x = this.x + colWidth;
+			};
+			break;
+			
+			case "down":
+			if(this.y<400){
+				this.y = this.y + rowHeight;
+			};
+			break;
+		};
+};
 
 // This function randomly selects a number from any number array passed in as the parameter.
 var randomNumFromArray = function(numberArray){
@@ -101,6 +132,7 @@ var randomNumFromArray = function(numberArray){
 
 // Now instantiate your objects.
 
+
 // Place all enemy objects in an array called allEnemies
 var allEnemies = [];
 
@@ -109,10 +141,8 @@ for(var i=0; i<enemyPopulation; i++)
 	allEnemies.push(new Enemy);
 };
 
-
 // Place the player object in a variable called player.
 var player = new Player;
-
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
